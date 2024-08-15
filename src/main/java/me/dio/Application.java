3d5b79fb5ec1a778f3553f76eafd1b52f4a4af7b
AnderAlmeida123 <anderson_raft@hotmail.com>
2024-08-14 23:54:49 -0300

    Initial commit

---
+++ src/main/java/me/dio/Application.java
@@ -0,0 +1,13 @@
+package me.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoJavaComIaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoJavaComIaApplication.class, args);
+	}
+
+}
